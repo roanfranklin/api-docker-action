4fd3dce33984c1b0895a9d2fbcec45911641061e
Roan Franklin <roanfranklin@gmail.com>
2022-10-20 14:22:15 -0300

    update status erros

--- entrypoint.py
+++ entrypoint.py
@@ -3,6 +3,7 @@
 import requests
 import base64
 import os
+import github_action_utils as gha_utils
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,41 +19,41 @@ USERNAMEREGISTRY = os.getenv('USERNAMEREGISTRY')
 PASSWORDREGISTRY = os.getenv('PASSWORDREGISTRY')
 
 if not URL:
-    print('URL is not set. Quitting.')
+    gha_utils.error('URL is not set. Quitting.')
     quit()
 
 if not USERNAME:
-    print('USERNAME is not set. Quitting.')
+    gha_utils.error('USERNAME is not set. Quitting.')
     quit()
 
 if not PASSWORD:
-    print('PASSWORD is not set. Quitting.')
+    gha_utils.error('PASSWORD is not set. Quitting.')
     quit()
 
 if not STACK:
-    print('STACK is not set. Quitting.')
+    gha_utils.error('STACK is not set. Quitting.')
     quit()
 
 if not ENVIRONMENT:
-    print('ENVIRONMENT is not set. Quitting.')
+    gha_utils.error('ENVIRONMENT is not set. Quitting.')
     quit()
 else:
     ENVIRONMENT = ENVIRONMENT.split(';')
 
 if not DOCKERCOMPOSE:
-    print('DOCKERCOMPOSE is not set. Quitting.')
+    gha_utils.error('DOCKERCOMPOSE is not set. Quitting.')
     quit()
 
 if not URLREGISTRY:
-    print('URLREGISTRY is not set. Quitting.')
+    gha_utils.error('URLREGISTRY is not set. Quitting.')
     quit()
 
 if not USERNAMEREGISTRY:
-    print('USERNAMEREGISTRY is not set. Quitting.')
+    gha_utils.error('USERNAMEREGISTRY is not set. Quitting.')
     quit()
 
 if not PASSWORDREGISTRY:
-    print('PASSWORDREGISTRY is not set. Quitting.')
+    gha_utils.error('PASSWORDREGISTRY is not set. Quitting.')
     quit()
 
 
